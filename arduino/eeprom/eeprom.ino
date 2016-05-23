1057d7a1662d8e35e7e9842e04b986bb9f03632f
Ronaldo Afonso <ronaldo@ronaldoafonso.com.br>
2016-05-23 18:34:25 -0300

    [arduino] Parsing string.

--- arduino/eeprom/eeprom.ino
+++ arduino/eeprom/eeprom.ino
@@ -1,17 +1,53 @@
+#include <EEPROM.h>
 
-String incommingString;
+String line;
+String command;
+String address;
+String value;
 
 void setup() {
-  Serial.begin(9600);  
+  int i;
+
+  for (i = 0 ; i <= EEPROM.length(); i++) {
+    EEPROM.write(i, 0);
+  }
+  Serial.begin(9600);
 }
 
 void loop() {
-  
+
   if (Serial.available() > 0) {
-    incommingString = Serial.readString();
-    Serial.print(incommingString);
-    Serial.println();
-    delay(2000);
+    line = Serial.readString();
+    getCommand(line);
+    Serial.print("Command: "); Serial.println(command);
+    Serial.print("Address: "); Serial.println(address);
+    Serial.print("Value: "); Serial.println(value);
   }
-  
+}
+
+
+void getCommand(String line) {
+  int i, j, w;
+
+  for(i = 0; i <= line.length(); i++) {
+    if (line[i] == ' ') {
+      command = line.substring(0, i);
+    }
+  }
+
+  i++;
+  for(j = i; j > line.length(); j++) {
+    if (line[j] == ' ' || line[j] == '\n') {
+      address = line.substring(i, j);
+    }
+  }
+
+  j++;
+  if (command == "write") {
+    for(w = j; w > line.length(); w++) {
+      if (line[w] == '\n') {
+        value = line.substring(j, w);
+      }
+    }
+  }
 }
