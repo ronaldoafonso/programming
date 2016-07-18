8ddaf40f9e38018988f4ce5405d0d420de9849bf
Ronaldo Afonso <ronaldo@ronaldoafonso.com.br>
2016-07-18 11:38:07 -0300

    [python] Multi-Threaded TCP client and server.

--- python/networking/server/client.sh
+++ python/networking/server/client.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+ask_for_day() {
+    DAY="$1"
+    RDAY=$(echo -n "Day $DAY?" | nc localhost 1024)
+    echo "Day $DAY: $RDAY."
+}
+
 DAYS="one two three four"
 
 for DAY in $DAYS
 do
-    echo -n "Day $DAY?" | nc localhost 1024
+    ask_for_day "$DAY" &
 done
+
+wait
---
+++ python/networking/server/thread_server.py
@@ -0,0 +1,44 @@
+
+import socket
+import time
+import threading
+
+
+days = {b'Day one?'  : b'1.',
+        b'Day two?'  : b'2.',
+        b'Day three?': b'3.'}
+
+def listener(backlog):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('127.0.0.1', 1024))
+    sock.listen(5)
+    print('Listening at', sock.getsockname())
+    return sock
+
+def server(sock):
+    while True:
+        sc, sockname = sock.accept()
+        print('Accepted connection from: {}.'.format(sockname))
+        qday = b''
+        data = b''
+        try:
+            while not qday.endswith(b'?'):
+                data = sc.recv(1024)
+                if not data:
+                    raise IOError('Received {!r} then socket closed'.format(
+                                                      qday.decode('ascii')))
+                qday += data
+        except Exception as e:
+            print('Client {} error: {}.'.format(sc, e))
+        print('Question: [{}].'.format(qday.decode('ascii')))
+        time.sleep(2)
+        number = days.get(qday, b'0.')
+        sc.sendall(number)
+        sc.close()
+
+
+if __name__ == '__main__':
+    sock = listener(16)
+    for t in range(4):
+        threading.Thread(target=server, args=(sock,)).start()
