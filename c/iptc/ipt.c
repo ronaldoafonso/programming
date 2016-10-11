f1b392e48ddfbe7150100702c8864c46c7ce73ab
Ronaldo Afonso <ronaldo@ronaldoafonso.com.br>
2016-10-11 18:13:05 -0300

    [c] Adding a rule to iptables.

--- c/iptc/ipt.c
+++ c/iptc/ipt.c
@@ -3,13 +3,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
+#include <arpa/inet.h>
 #include <libiptc/libiptc.h>
 
+
+typedef struct _Entry {
+    struct ipt_entry entry;
+    struct xt_standard_target target;
+} Entry;
 
 int main(int argc, char *argv[])
 {
     struct xtc_handle *handle;
-    const struct ipt_entry *entry = NULL;
+    unsigned int dst_ip;
+    Entry entry;
+    int rc;
+    const struct ipt_entry *pentry = NULL;
     struct ipt_counters *cnt;
     int i = 0;
 
@@ -19,8 +29,36 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    entry = iptc_first_rule("OUTPUT", handle);
-    if (entry) {
+    memset(&entry, 0, sizeof(Entry));
+
+    entry.target.target.u.user.target_size = XT_ALIGN(sizeof(struct xt_standard_target));
+    strncpy(entry.target.target.u.user.name, "ACCEPT", sizeof(entry.target.target.u.user.name));
+
+    entry.entry.target_offset = sizeof(struct ipt_entry);
+    entry.entry.next_offset = entry.entry.target_offset + entry.target.target.u.user.target_size;
+
+    inet_pton(AF_INET, "1.1.1.1", &dst_ip);
+    entry.entry.ip.dst.s_addr = dst_ip;
+    entry.entry.ip.dmsk.s_addr = 0xFFFFFFFF;
+
+    rc = iptc_append_entry("OUTPUT", (struct ipt_entry *) &entry, handle);
+    if (!rc) {
+        printf("Error: %s.\n", iptc_strerror(errno));
+        iptc_free(handle);
+        exit(EXIT_FAILURE);
+    }
+
+    rc = iptc_commit(handle);
+    if (!rc) {
+        printf("Error: %s.\n", iptc_strerror(errno));
+        iptc_free(handle);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Appended rule to: %d.\n", rc);
+
+    pentry = iptc_first_rule("OUTPUT", handle);
+    if (pentry) {
         do {
             i += 1;
             cnt = iptc_read_counter("OUTPUT", i, handle);
@@ -28,8 +66,8 @@ int main(int argc, char *argv[])
                                                              i,
                                                              cnt->pcnt,
                                                              cnt->bcnt);
-            entry = iptc_next_rule(entry, handle);
-        } while (entry);
+            pentry = iptc_next_rule(pentry, handle);
+        } while (pentry);
     }
 
     exit(EXIT_SUCCESS);
