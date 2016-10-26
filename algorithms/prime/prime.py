aecb060d5799ecf849198e73e2de7fa6280b7e26
Ronaldo Afonso <ronaldo@ronaldoafonso.com.br>
2016-10-26 13:35:25 -0200

    [algorithms] Checking if a number is prime.

---
+++ algorithms/prime/prime.py
@@ -0,0 +1,14 @@
+
+def is_prime(n):
+    prime = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            prime = prime + 1
+        if prime > 2:
+            return False
+    return True
+
+
+for i in range(1, 50):
+    if is_prime(i):
+        print("{0} is prime.".format(i))
