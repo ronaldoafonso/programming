f0a379448e79a7914c979f744ea081e1f515366c
Ronaldo Afonso <ronaldo@ronaldoafonso.com.br>
2016-11-03 18:54:11 -0200

    [algorithms] A greedy example.

---
+++ algorithms/greedy/fknapsack.py
@@ -0,0 +1,33 @@
+# Uses python3
+
+import sys
+
+def get_optimal_value(capacity, weights, values):
+    loot = 0.0
+    vw = []
+    for i in range(0, len(weights)):
+        vw.append([values[i]/weights[i], values[i], weights[i]])
+    vw.sort(reverse=True)
+    for i in vw:
+        while i[2]:
+            if capacity >= i[2]:
+                capacity = capacity - i[2]
+                i[2] = 0
+                loot = loot + i[1]
+            elif capacity < i[2] and capacity:
+                frac = capacity / i[2]
+                capacity = 0
+                i[2] = 0
+                loot = loot + (i[1] * frac)
+        if capacity == 0:
+            break
+    return loot
+
+
+if __name__ == "__main__":
+    data = list(map(int, sys.stdin.read().split()))
+    n, capacity = data[0:2]
+    values =  [i for i in data[2::2]]
+    weights = [i for i in data[3::2]]
+    opt_value = get_optimal_value(capacity, weights, values)
+    print("{:.4f}".format(opt_value))
