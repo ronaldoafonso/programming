fddf664067186ed5a28a5af6b7fa3701ba168437
Ronaldo Afonso <ronaldo@ronaldoafonso.com.br>
2016-11-08 16:38:22 -0200

    [c] Capturing packets in a loop.

--- c/pcap/tpcap.c
+++ c/pcap/tpcap.c
@@ -6,6 +6,21 @@
 #include <pcap.h>
 
 
+void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+{
+    static unsigned int count = 0;
+    int i;
+
+    if (packet) {
+        fprintf(stdout, "%u: Header length: %d.\n", count++, header->len);
+        for (i = 0; i < header->len; i++) {
+            fprintf(stdout, "[0x%x]", packet[i]);
+        }
+        fprintf(stdout, "\n");
+    }
+}
+
+
 int main(int argc, char *argv[])
 {
     char device[] = "wlan0";
@@ -13,15 +28,11 @@ int main(int argc, char *argv[])
     pcap_t *handle;
     struct bpf_program fp;
     char filter[64];
-    struct pcap_pkthdr header;
-    const u_char *packet;
 
     memset(error, '\0', 64);
     memset(&fp, 0, sizeof(struct bpf_program));
     memset(filter, '\0', 64);
     strncpy(filter, "wlan type mgt subtype probe-req", 31);
-    memset(&header, 0, sizeof(struct pcap_pkthdr));
-    packet = NULL;
 
     handle = pcap_open_live(device, BUFSIZ, 1, 0, error);
     if (!handle) {
@@ -39,15 +50,7 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    packet = pcap_next(handle, &header);
-    if (packet) {
-        int i;
-        fprintf(stdout, "Header length: %d.\n", header.len);
-        for (i = 0; i < header.len; i++) {
-            fprintf(stdout, "[0x%x]", packet[i]);
-        }
-        fprintf(stdout, "\n");
-    }
+    pcap_loop(handle, -1, got_packet, NULL);
 
     pcap_close(handle);
     exit(EXIT_SUCCESS);
