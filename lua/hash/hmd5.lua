bf17a01b0b8275e734f7f2f02a91ba814a48a407
Ronaldo Afonso <ronaldo@ronaldoafonso.com.br>
2016-11-28 15:15:13 -0200

    [lua] Using sum stead of crypt.

--- lua/hash/hmd5.lua
+++ lua/hash/hmd5.lua
@@ -4,7 +4,7 @@ local md5 = require("md5")
 local msg1 = "Ronaldo Afonso"
 local msg2 = "Ronaldo Afonso"
 
-if md5.crypt(msg1, "") == md5.crypt(msg2, "") then
+if md5.sum(msg1) == md5.sum(msg2) then
     print("The same messages.")
 else
     print("Different messages.")
